4815b3c7a831076408796a2620c9675b81e24001
bbrewer-pillar <bbrewer@pillartechnology.com>
2018-10-26 16:00:17 -0400

    SMRT-610 - reflect when link has been copied
    co-authored-by: Alex Markley <amarkley@pillartechnology.com>

--- src/components/share/share.js
+++ src/components/share/share.js
@@ -1,5 +1,6 @@
 import './share.scss'
 
+import React from 'react'
 import linkIcon from '../../assets/link.svg'
 import {
   FacebookShareButton,
@@ -12,39 +13,48 @@ import {
 } from 'react-share';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
-const Share = props => {
-  const shareLocation = location.href
-  const iconSize = 40
-  const iconRound = true
+class Share extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copyMessage: 'Copy Link'
+    }
+  }
 
-  return (
-    <share-zone>
-      <div className='header'>SHARE DATASET</div>
+  render() {
+    const shareLocation = location.href
+    const iconSize = 40
+    const iconRound = true
 
-      <TwitterShareButton url={shareLocation} className="twitter button">
-        <TwitterIcon round={iconRound} size={iconSize} />
-        <div className="share">Tweet</div>
-      </TwitterShareButton>
+    return (
+      <share-zone>
+        <div className='header'>SHARE DATASET</div>
 
-      <FacebookShareButton url={shareLocation} className="facebook button">
-        <FacebookIcon round={iconRound} size={iconSize} />
-        <div className="share">Share</div>
-        <FacebookShareCount url={shareLocation} className="share-count"/>
-      </FacebookShareButton>
+        <TwitterShareButton url={shareLocation} className="twitter button">
+          <TwitterIcon round={iconRound} size={iconSize} />
+          <div className="share">Tweet</div>
+        </TwitterShareButton>
 
-      <LinkedinShareButton url={shareLocation} className="linkedin button">
-        <LinkedinIcon round={iconRound} size={iconSize} />
-        <div className="share">Share</div>
-      </LinkedinShareButton>
-          
-      <CopyToClipboard text={shareLocation}>
-        <button tabIndex="0" className='clipboard button'>
-          <img style={{width: `${iconSize/2}px`, height: `${iconSize}px`}} src={linkIcon}/>
-          <div className="share">Copy Link</div>
-        </button>
-      </CopyToClipboard>
-    </share-zone>
-  )
+        <FacebookShareButton url={shareLocation} className="facebook button">
+          <FacebookIcon round={iconRound} size={iconSize} />
+          <div className="share">Share</div>
+          <FacebookShareCount url={shareLocation} className="share-count"/>
+        </FacebookShareButton>
+
+        <LinkedinShareButton url={shareLocation} className="linkedin button">
+          <LinkedinIcon round={iconRound} size={iconSize} />
+          <div className="share">Share</div>
+        </LinkedinShareButton>
+
+        <CopyToClipboard text={shareLocation} onCopy={() => this.setState({copyMessage:"Copied!"})}>
+          <button tabIndex="0" className='clipboard button'>
+            <img style={{width: `${iconSize/2}px`, height: `${iconSize}px`}} src={linkIcon}/>
+            <div className="share">{this.state.copyMessage}</div>
+          </button>
+        </CopyToClipboard>
+      </share-zone>
+    )
+  }
 }
 
 export default Share
