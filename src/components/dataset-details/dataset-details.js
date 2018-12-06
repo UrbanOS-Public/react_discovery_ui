c3d8d8d1e19a582cff675c9dc701fa63d2e81231
Austin <raustinkucera@gmail.com>
2018-12-06 09:30:06 -0500

    SMRT-687 Dataset details page doesn't display the tag block when there are no tags (#21)
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>

--- src/components/dataset-details/dataset-details.js
+++ src/components/dataset-details/dataset-details.js
@@ -7,10 +7,12 @@ const DatasetDetails = props => {
     <dataset-details>
       <div className='name'>{props.dataset.name}</div>
       <div className='description'>{props.dataset.description}</div>
-      <div className='tags'>
-        <div className='tag-label'>TAGS</div>
-        {props.dataset.tags.map(createTag)}
-      </div>
+      { props.dataset.tags &&
+        <div className='tags'>
+          <div className='tag-label'>TAGS</div>
+          {props.dataset.tags.map(createTag)}
+        </div>
+      }
       <div className='data-and-resources-header'>Data & Resources</div>
       <div className='resources'>{props.dataset.resources}</div>
     </dataset-details>
--- src/components/dataset-details/dataset-details.test.js
+++ src/components/dataset-details/dataset-details.test.js
@@ -8,4 +8,9 @@ describe('data card element', () => {
     subject = shallow(<DatasetDetails dataset={{ tags: [{ name: 'COTA' }, { name: 'Transit Stops' }] }} />)
     expect(subject.find('.tag').length).toEqual(2)
   })
+
+  test('card to render text based on props', () => {
+    subject = shallow(<DatasetDetails dataset={{ tags: null }} />)
+    expect(subject.find('.tags').length).toEqual(0)
+  })
 })
