7fdcadbf6423bcf8f64662ca58c3900e17c32822
Benjamin Brewer <benjamin.brewer@accenture.com>
2019-04-03 11:17:20 -0400

    SMRT-1014 - Organization image now links back to search with organization selected.
    co-authored-by: Nathaniel Stevens <nstevens@pillartechnology.com>

--- src/components/organization/organization.js
+++ src/components/organization/organization.js
@@ -1,9 +1,12 @@
 import './organization.scss'
+import { QueryStringBuilder } from '../../utils'
 
 export default ({ organization }) => (
   <dataset-organization>
     <div className='header'>Organization Info</div>
-    <img className='logo' src={organization.image} />
+    <a href={`/?${QueryStringBuilder.createFilterQueryString('organization', organization.name)}`}>
+      <img className='logo' src={organization.image} />
+    </a>
     <div className='name'>{organization.name}</div>
     <div className='description'>{organization.description}</div>
   </dataset-organization>
