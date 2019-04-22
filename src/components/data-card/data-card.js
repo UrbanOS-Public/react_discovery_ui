d602285c033afda700730ec1df38334af876111d
Timothy Regan <tregan@pillartechnology.com>
2019-04-22 15:23:23 -0400

    SMRT-1174 Truncate description and render html
    co-authored-by: Timothy Regan <tregan@pillartechnology.com>

--- src/components/data-card/data-card.js
+++ src/components/data-card/data-card.js
@@ -1,26 +1,40 @@
-import './data-card.scss'
-import { Link } from 'react-router-dom'
-import moment from 'moment'
+import "./data-card.scss";
+import { Link } from "react-router-dom";
+import moment from "moment";
 
 const DataCard = props => {
-  const fileTypes = (fileTypes) => {
-    return fileTypes.map(fileType => <span key={fileType} className='file-type'>{fileType} </span>)
-  }
+  const fileTypes = fileTypes => {
+    return fileTypes.map(fileType => (
+      <span key={fileType} className="file-type">
+        {fileType}{" "}
+      </span>
+    ));
+  };
 
-  const dataset = props.dataset
+  const dataset = props.dataset;
   return (
     <data-card>
-      <Link className='title' to={`/dataset/${dataset.organization_name}/${dataset.name}`}>
+      <Link
+        className="title"
+        to={`/dataset/${dataset.organization_name}/${dataset.name}`}
+      >
         {dataset.title}
       </Link>
-      <div className='description'>{dataset.description}</div>
-      <div className='card-metadata'>
-        <div className='last-modified'>Updated {moment(dataset.modifiedTime).format('MMM DD, YYYY')}</div>
-        <div className='separator'>•</div>
-        <div className='file-types'>File Type: {fileTypes(dataset.fileTypes)} </div>
+      <div className="description">
+        {dataset.description.substring(0, 240)}
+        {dataset.description.length > 240 && <span> ...</span>}
+      </div>
+      <div className="card-metadata">
+        <div className="last-modified">
+          Updated {moment(dataset.modifiedTime).format("MMM DD, YYYY")}
+        </div>
+        <div className="separator">•</div>
+        <div className="file-types">
+          File Type: {fileTypes(dataset.fileTypes)}{" "}
+        </div>
       </div>
     </data-card>
-  )
-}
+  );
+};
 
-export default DataCard
+export default DataCard;
--- src/components/dataset-details/dataset-details.js
+++ src/components/dataset-details/dataset-details.js
@@ -1,24 +1,38 @@
-import './dataset-details.scss'
-import { QueryStringBuilder } from '../../utils'
-import _ from 'lodash'
+import "./dataset-details.scss";
+import { QueryStringBuilder } from "../../utils";
+import _ from "lodash";
 
 const DatasetDetails = props => {
-  if (!props.dataset) { return <div /> }
+  if (!props.dataset) {
+    return <div />;
+  }
 
   return (
     <dataset-details>
-      <div className='name'>{props.dataset.title}</div>
-      <div className='description'>{props.dataset.description}</div>
-      {!_.isEmpty(props.dataset.keywords) &&
-        <div className='keywords'>
-          <div className='keyword-label'>KEYWORDS</div>
+      <div className="name">{props.dataset.title}</div>
+      <div
+        className="description"
+        dangerouslySetInnerHTML={{ __html: props.dataset.description }}
+      />
+
+      {!_.isEmpty(props.dataset.keywords) && (
+        <div className="keywords">
+          <div className="keyword-label">KEYWORDS</div>
           {props.dataset.keywords.map(createKeyword)}
         </div>
-      }
+      )}
     </dataset-details>
-  )
-}
+  );
+};
 
-const createKeyword = (name) => <a key={`dataset-keyword-${name}`} className='keyword' href={`/?${QueryStringBuilder.createFilterQueryString('keywords', name)}`}>{name}</a>
+const createKeyword = name => (
+  <a
+    key={`dataset-keyword-${name}`}
+    className="keyword"
+    href={`/?${QueryStringBuilder.createFilterQueryString("keywords", name)}`}
+  >
+    {name}
+  </a>
+);
 
-export default DatasetDetails
+export default DatasetDetails;
