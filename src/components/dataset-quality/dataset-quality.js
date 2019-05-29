088bbbeb8a2de1ebd64dfa675e513b429a46e086
Nathaniel Stevens <nstevens@pillartechnology.com>
2019-05-29 13:44:55 -0400

    SMRT-1004 Adds styling to dataset-quality
    co-authored-by: Paul Linville <plinville@pillartechnology.com>

--- src/components/dataset-quality/dataset-quality.js
+++ src/components/dataset-quality/dataset-quality.js
@@ -12,7 +12,7 @@ export default class extends Component {
           <div className="completeness-score">
             {Math.round(this.props.completeness * 100)}%
           </div>
-          <div>Dataset quality is a measure of the completeness of the dataset</div>
+          <div className="completeness-description">Dataset quality is a measure of the completeness of the dataset</div>
         </div>
       : <div />
     )
@@ -21,7 +21,6 @@ export default class extends Component {
   render() {
     return (
       <dataset-quality>
-        <div className="foo" />
         <CollapsableBox title={"Dataset Quality"} headerHtml={this.streamingHeader()} bodyHtml={<div />} expanded={false} />
       </dataset-quality>
     )
