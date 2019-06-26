c0a576f8c8bdf593e50401d06dc96122a5ba87e7
Timothy Regan <tregan@pillartechnology.com>
2019-06-26 15:26:44 -0400

    Simplified export

--- src/pages/dataset-list-view/index.js
+++ src/pages/dataset-list-view/index.js
@@ -1,4 +1,3 @@
-import DatasetListView from './dataset-list-view.js'
 import DatasetListViewWrapper from "./dataset-list-view-wrapper.js"
 
-export default { DatasetListViewWrapper }
+export default DatasetListViewWrapper
