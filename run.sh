8977f56f3cbf8c8358e5c28c88aa57f83f8a4da6
JarredOlson <jarred.olson@accenture.com>
2019-07-22 15:30:36 -0400

    fixing geojson rendering
    co-authored-by: Paul Linville <plinville@pillartechnology.com>

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ cat > /usr/share/nginx/html/config.js <<EOL
 window.API_HOST = '${API_HOST}'
 window.GTM_ID = '${GTM_ID}'
 window.BASE_URL = '${BASE_URL}'
+window.STREETS_TILE_LAYER_URL = '${STREETS_TILE_LAYER_URL}'
 EOL
 
 nginx -g "daemon off;"
