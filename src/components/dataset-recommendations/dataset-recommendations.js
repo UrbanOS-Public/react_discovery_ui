02e8a72875b3498c1988a6afa8c1983905062531
Jessie Morris <jmorris2@pillartechnology.com>
2019-10-07 16:38:48 -0400

    working with urls

--- src/components/dataset-recommendations/dataset-recommendations.js
+++ src/components/dataset-recommendations/dataset-recommendations.js
@@ -9,7 +9,7 @@ export default (props) => {
         {<ul>
           {props.recommendations.map(rec =>
             <li key={rec.id}>
-              rec.systemName
+              <a href={"/dataset/" + rec.systemName.split("__")[0] + "/" + rec.systemName.split("__")[1]}>{rec.systemName}</a>
             </li>)}
         </ul>}
       </CollapsableBox>
