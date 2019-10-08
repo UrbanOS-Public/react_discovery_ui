20f90471efa175be8788e9ffbdbab697da8d6e1a
jakeprem <jakeprem@users.noreply.github.com>
2019-10-08 13:03:25 -0400

    Bug fix refactor (#46)
    * Begin pulling common dataset state and logic into a new dataset-selectors file
    * Refactor business logic out of components and into selectors
    
    co-authored-by: Jake Prem <jakeprem@gmail.com>

---
+++ src/store/dataset-selectors.js
@@ -0,0 +1,43 @@
+import { createSelector } from "reselect";
+
+const SOURCE_TYPE = {
+  STREAMING: "stream",
+  INGEST: "ingest",
+  REMOTE: "remote",
+  HOST: "host"
+};
+
+const dataset = state => state.datasetReducer.dataset;
+const isStreamingDataset = state =>
+  dataset(state).sourceType === SOURCE_TYPE.STREAMING;
+const isIngestDataset = state =>
+  dataset(state).sourceType === SOURCE_TYPE.INGEST;
+const isRemoteDataset = state =>
+  dataset(state).sourceType === SOURCE_TYPE.REMOTE;
+const isHostDataset = state => dataset(state).sourceType === SOURCE_TYPE.HOST;
+const isQueryableDataset = state =>
+  isIngestDataset(state) || isStreamingDataset(state);
+
+const isCsvDataset = createSelector(
+  dataset,
+  dataset => {
+    return dataset.sourceFormat && dataset.sourceFormat.toLowerCase() === "csv";
+  }
+);
+
+const isGeoJSONDataset = createSelector(
+  dataset,
+  isRemoteDataset,
+  (dataset, isRemote) => dataset.sourceFormat === "geojson" && !isRemote
+);
+
+export {
+  dataset as getDataset,
+  isStreamingDataset,
+  isIngestDataset,
+  isRemoteDataset,
+  isHostDataset,
+  isQueryableDataset,
+  isCsvDataset,
+  isGeoJSONDataset
+};
