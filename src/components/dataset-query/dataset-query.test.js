e5e2a59c3d5f1aef0a994e43d56d086d01119db1
Nathaniel Stevens <nstevens@pillartechnology.com>
2019-10-15 12:48:52 -0400

    Fixes styling and test names

--- src/components/dataset-query/dataset-query.test.js
+++ src/components/dataset-query/dataset-query.test.js
@@ -3,7 +3,8 @@ import ReactTooltip from 'react-tooltip'
 import DatasetQuery from './dataset-query'
 import LoadingElement from '../generic-elements/loading-element';
 import { recommendations } from '../../../test-helpers/recommendations'
-import AssignmentOutlinedIcon from '@material-ui/icons/AssignmentOutlined'
+import { act } from 'react-dom/test-utils';
+
 
 let subject, queryCallback, updateCallback
 
@@ -169,6 +170,17 @@ describe('DatasetQuery', () => {
         expect(subject.contains(ReactTooltip)).toBeTruthy()
         const tooltips = subject.find(ReactTooltip).map((elem) => elem.text())
         expect(tooltips).toContain(`Copy table name '${recommendations[0].systemName}'`)
+      })
+
+      test('copy button changes tooltip text to "Copied" when it is clicked', () => {
+        subject.find('.copy-table-name-icon').at(0).simulate('click')
+        expect(subject.find(ReactTooltip).at(0).text()).toEqual("Copied!")
+      })
+
+      test('tooltip changes back to table name text after it disapears', () => {
+        subject.find('.copy-table-name-icon').at(0).simulate('click')
+        act(() => { subject.find(ReactTooltip).at(0).prop('afterHide')() })
+        expect(subject.find(ReactTooltip).at(0).text()).toEqual(`Copy table name '${recommendations[0].systemName}'`)
       })
     })
 
