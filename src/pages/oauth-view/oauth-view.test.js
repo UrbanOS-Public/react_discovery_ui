f462c4f67747a793679f7f52c9a3c77da501fc68
cameronmarsh <cameron.marsh@accenture.com>
2019-10-31 10:16:14 -0400

    Add test for oauth view
    smartcitiesdata/react_discovery_ui#69

--- src/pages/oauth-view/oauth-view.test.js
+++ src/pages/oauth-view/oauth-view.test.js
@@ -1,47 +1,91 @@
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
+import { act } from 'react-dom/test-utils'
 import OAuthView from './oauth-view'
+import { default as createAuth0Client } from '@auth0/auth0-spa-js'
 
+jest.mock('@auth0/auth0-spa-js')
+jest.mock('axios')
 
-const runUseEffect = () => {
-  const useEffect = jest.spyOn(React, "useEffect")
-  useEffect.mockImplementation(f => f())
-}
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
+
+afterAll(() => {
+  console.error = originalError
+})
 
 describe('OAuth View', () => {
-  let subject, callLoggedInHandler
+  let subject
+  const callLoggedInHandler = jest.fn()
 
-  const { location } = window
+  createAuth0Client.mockImplementation(() => ({
+    isAuthenticated: jest.fn(() => Promise.resolve(false)),
+    handleRedirectCallback: jest.fn(() => Promise.resolve({}))
+  }))
 
-  beforeEach(() => {
-    callLoggedInHandler = jest.fn()
-    subject = createSubject({ callLoggedIn: callLoggedInHandler })
-    delete window.location
-  })
-
-  afterAll(() => {
-    window.location = location
-  })
-
-  //should this be in the saga test?
   describe('upon successful login', () => {
+    beforeEach(() => {
+      subject = createSubject({
+        callLoggedIn: callLoggedInHandler,
+        history: {
+          location: {
+            search: 'code=someauth0tokenstring'
+          }
+        }
+      })
+
+    })
+
     it('calls the logged-in endpoint', () => {
-      // mock window location
-      window.location = { search: "code=" }
       expect(callLoggedInHandler).toHaveBeenCalled()
+    })
+  })
+
+
+  describe('upon initial render', () => {
+    beforeEach(() => {
+      subject = createSubject({
+        callLoggedIn: callLoggedInHandler,
+        history: {
+          location: {
+            search: ''
+          }
+        }
+      })
+    })
+
+    it('initializes with the correct domain and client ID', () => {
+      expect(createAuth0Client).toBeCalledWith({
+        domain: window.AUTH0_DOMAIN,
+        client_id: window.AUTH0_CLIENT_ID,
+        audience: window.AUTH0_AUDIENCE,
+        redirect_uri: `${window.location.origin}/oauth`
+      })
+
     })
   })
 })
 
 const createSubject = (props = {}) => {
   const defaultProps = {
-    callLoggedIn: jest.fn()
+    callLoggedIn: jest.fn(),
+    history: {}
   }
 
   const propsWithDefaults = Object.assign({}, defaultProps, props)
 
-  return shallow(
-    <OAuthView
-      callLoggedIn={propsWithDefaults.callLoggedIn}
-    />
-  )
+  act(() => {
+    mount(
+      <OAuthView
+        callLoggedIn={propsWithDefaults.callLoggedIn}
+        history={propsWithDefaults.history}
+      />
+    )
+  })
 }
