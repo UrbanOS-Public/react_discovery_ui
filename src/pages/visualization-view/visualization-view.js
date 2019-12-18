1d6c1a8fa50d198a4ad21afb138c16460f1806b7
cameronmarsh <cmarsh@pillartechnology.com>
2019-12-18 11:27:06 -0500

    Clean up visualization and query data on page unmount
    smartcolumbusos/discovery_ui#26

--- src/pages/visualization-view/visualization-view.js
+++ src/pages/visualization-view/visualization-view.js
@@ -37,7 +37,7 @@ const VisualizationView = (props) => {
   const startIndex = idFromUrl ? 1 : 0
   const [index, setIndex] = useState(startIndex)
 
-  React.useEffect(() => { reset() }, [])
+  React.useEffect(() => { reset(); return function cleanup() { reset() }}, [])
   React.useEffect(() => { if (idFromUrl && idFromUrl !== idFromState) load(idFromUrl) }, [idFromUrl])
   React.useEffect(() => { if (idFromState && idFromUrl !== idFromState) history.push(linkUrl) }, [idFromState])
   React.useEffect(() => { setLocalTitle(title) }, [title])
