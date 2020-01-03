5a3390260a79db0bb95ef9afddd951267fc89cd9
Nathaniel Stevens <nstevens@pillartechnology.com>
2020-01-03 12:06:11 -0500

    Fixed bug where column names with dots broke dataset preview
    smartcitiesdata/smartcitiesdata/smartcitiesdata#206
    
    co-authored-by: Nathaniel Stevens <nstevens@pillartechnology.com>

--- src/components/dataset-preview/dataset-preview.test.js
+++ src/components/dataset-preview/dataset-preview.test.js
@@ -17,37 +17,43 @@ describe('dataset preview', () => {
             data: [{
               firstName: 'Joe',
               lastName: 'Smith',
-              enrolled: true
+              enrolled: true,
+              'firstName.lastName': 'Joe Smith'
             }, {
               firstName: 'Jane',
               lastName: 'Doe',
-              enrolled: false
+              enrolled: false,
+              'firstName.lastName': 'Jane Doe'
             }],
             meta: {
-              columns: ['firstName', 'lastName', 'enrolled']
+              columns: ['firstName', 'lastName', 'enrolled', 'firstName.lastName']
             }
           }
         }
       />)
     })
 
-    test('should render table headers', () => {
+    test('should render table headers including column names with dots in them', () => {
       const tableHeaderSelector = '.rt-resizable-header-content'
-      expect(subject.find(tableHeaderSelector).get(0).children[0].data).toEqual('firstName')
-      expect(subject.find(tableHeaderSelector).get(1).children[0].data).toEqual('lastName')
-      expect(subject.find(tableHeaderSelector).get(2).children[0].data).toEqual('enrolled')
-      expect(subject.find(tableHeaderSelector).length).toEqual(3)
+
+      const headers = subject.find(tableHeaderSelector)
+      const children = [...Array(4).keys()].map(x => headers.get(x).children[0].data)
+
+      expect(children).toContain('firstName', 'lastName', 'enrolled', 'firstName.lastName')
     })
 
-    test('should render table rows', () => {
+    test('should render table rows including data for column names with dots', () => {
       const tableElementSelector = '.rt-tr .rt-td'
-      expect(subject.find(tableElementSelector).get(0).children[0].data).toEqual('Joe')
-      expect(subject.find(tableElementSelector).get(1).children[0].data).toEqual('Smith')
-      expect(subject.find(tableElementSelector).get(2).children[0].data).toEqual('true')
+      const table_elements = subject.find(tableElementSelector)
+      const expected_rows = [
+        ['Joe', 'Smith', 'true', 'Joe Smith'],
+        ['Jane', 'Doe', 'false', 'Jane Doe']
+      ]
 
-      expect(subject.find(tableElementSelector).get(3).children[0].data).toEqual('Jane')
-      expect(subject.find(tableElementSelector).get(4).children[0].data).toEqual('Doe')
-      expect(subject.find(tableElementSelector).get(5).children[0].data).toEqual('false')
+      const actual_row1 = [0, 1, 2, 3].map(x => table_elements.get(x).children[0].data)
+      const actual_row2 = [4, 5, 6, 7].map(x => table_elements.get(x).children[0].data)
+      expect(actual_row1).toEqual(expected_rows[0])
+      expect(actual_row2).toEqual(expected_rows[1])
     })
   })
 
