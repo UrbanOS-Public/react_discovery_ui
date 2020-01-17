20e586f21c3e5574a33dc9d63ade2f7c2a26d2b3
kennyatpillar <kdrobnack@pillartechnology.com>
2020-01-17 14:52:01 -0500

    cleanup code and delete test

--- src/components/generic-elements/download-button/download-button.js
+++ src/components/generic-elements/download-button/download-button.js
@@ -1,27 +1,28 @@
 import './download-button.scss'
-import { createRef } from 'react'
+import { createRef, useState } from 'react'
+import React from 'react'
 import {AuthenticatedHTTPClient} from '../../../utils/http-clients'
 
 export function DownloadButton ({ url, format, filename }) {
-  const link = createRef()
-  const handleAction = async () => {
-    if (link.current.href) { 
-      console.log("href already set", link.current.href)
-      return 
-    }
 
+  const link = createRef()
+  const [downloadUrl, setDownloadUrl] = useState()
+  
+  React.useEffect(() => {
+    if (downloadUrl) {
+      link.current.click()
+    }
+  }, [downloadUrl])
+
+  const handleAction = async () => {
     const result = await AuthenticatedHTTPClient.get(url)
     const downloadUrl = window.API_HOST + "/api/v1" + result.data + "&format=" + format
-    
-    link.current.download = filename
-    link.current.href = downloadUrl      
-    link.current.click()
-    console.log("DONE with download", filename, downloadUrl)
+    setDownloadUrl(downloadUrl)
   }
 
   return (
     <download-button>
-      <a className='download-button' role='button' ref={link} rel='noopener noreferrer' onClick={handleAction}>Download</a>
+      <a className='download-button' role='button' ref={link} href={downloadUrl} download={filename} rel='noopener noreferrer' onClick={handleAction}>Download</a>
     </download-button>
   )
 
