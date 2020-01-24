a83b2e98bac67f7f6215c38e9fccd70348933732
jeffgrunewald <jeff@grunewalddesign.com>
2020-01-24 11:27:16 -0500

    updating websocket consumer example

--- src/components/streaming-api-doc/streaming-api-doc.js
+++ src/components/streaming-api-doc/streaming-api-doc.js
@@ -18,16 +18,19 @@ export default class extends Component {
       <div>
         <div className='example-container'>
           <div className='example-header'>
-            Example using <a href='https://github.com/esphen/wsta'>WSTA</a> on a terminal
+            Example using <a href='https://github.com/vi/websocat'>Websocat</a> on a terminal
             </div>
           <div className='example-code'>
-            <code>{`
-                              wsta -I --ping 50 --ping-msg
-                              '{"topic": "phoenix","event":"heartbeat","payload":{},"ref":"1"}'
-                              'wss://streams.${window.BASE_URL}/socket/websocket'
-                              '{"topic": "streaming:${systemName}","event":"phx_join","payload":{},"ref":"1"}'
-                          `}
+            <code>
+              <div>{`websocat wss://streams.${window.BASE_URL}/socket/websocket`}</div>
+              <div>{`'{"topic": "streaming:${systemName}","event":"phx_join","payload":{},"ref":"1"}'`}</div>
             </code>
+          </div>
+          <div className='example-header'>
+              Phoenix Channels clients
+          </div>
+          <div>
+            For application development, look at the <a href="https://www.npmjs.com/package/phoenix">official Phoenix Javascript library</a>, or one of the <a href="https://hexdocs.pm/phoenix/channels.html#client-libraries">other client libraries</a>
           </div>
         </div>
       </div>
