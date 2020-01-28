3cf2084397dee06546316e2470fba7100f3defa0
Matt Dunson <dunson.142000@gmail.com>
2020-01-28 14:58:04 -0500

    cypress tests for saved visualizations for non-logged-in users

---
+++ cypress/support/visualization_page.js
@@ -0,0 +1,16 @@
+export const Selectors = {
+    errorText: '[data-testid=error-text]'
+}
+
+export const Routes = {
+    info: {
+        method: 'GET',
+        url: '/sockjs-node/*',
+        response: 'fixture:info.json'
+    },
+    user: {
+        method: 'GET',
+        url: '/api/v1/visualization',
+        response: []
+    }
+}
