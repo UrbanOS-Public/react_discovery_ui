54307c65a55afe7c3c290de6390fd2de81439e02
Timothy Regan <tregan@pillartechnology.com>
2020-06-09 07:36:17 -0400

    remove commented code

--- src/pages/query-view/query-view.js
+++ src/pages/query-view/query-view.js
@@ -3,7 +3,6 @@ import React from "react";
 
 import QueryForm from "../../components/query-form";
 import DataView from "../../components/data-view";
-import ReactTable from "react-table";
 import LoadingElement from "../../components/generic-elements/loading-element";
 import _ from 'lodash'
 
@@ -28,9 +27,6 @@ const QueryView = props => {
       executeQuery(freestyleQueryText)
     }
   }, [shouldAutoExecuteQuery])
-
-  // const columns = determineColumns(dataSources)
-  // const data = getCleanData(queryData)
 
   const numRecords = queryData ? queryData.length + " records returned" : "";
 
@@ -63,41 +59,5 @@ const QueryView = props => {
     </query-view>
   );
 };
-
-const cleanseData = data => {
-  return data.map(row => cleanseRow(row));
-};
-
-const cleanseRow = row => {
-  const deconstructedObject = Object.entries(row);
-  const listOfKeyValues = deconstructedObject.map(field => ({
-    [field[0]]: cleanseField(field[1])
-  }));
-  const reconstructedObject = Object.assign({}, ...listOfKeyValues);
-
-  return reconstructedObject;
-};
-
-const cleanseField = value => {
-  if (typeof value === "boolean") {
-    return value.toString();
-  } else if (_.isNull(value) || _.isNaN(value)) {
-    return ''
-  } else if (typeof value === "object") {
-    return JSON.stringify(value);
-  } else {
-    return value;
-  }
-};
-
-const determineColumns = dataSources => {
-  return Object.keys(dataSources).map(col => {
-    return { Header: col, id: col, accessor: (row) => row[col], headerClassName: "table-header" };
-  })
-}
-
-const getCleanData = queryData => {
-  return queryData ? cleanseData(queryData) : queryData
-}
 
 export default QueryView;
