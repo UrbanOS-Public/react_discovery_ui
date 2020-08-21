bd5ea87a381dc2f9acda4ee062d756ecc80445c7
Matt Dunson <dunson.142000@gmail.com>
2020-08-21 16:48:37 -0400

    Add link home to search results when no datasets are found

--- cypress/integration/dataset_search_page_spec.js
+++ cypress/integration/dataset_search_page_spec.js
@@ -70,6 +70,13 @@ describe('Search interactions on the page', function () {
     cy.contains(titleOfFirstDataset)
   })
 
+  it.only('returns a link to the search page if 0 datasets are found', function () {
+    cy.route(routes.catarynDatasets)
+    cy.get('[data-testid=search]').clear().type('cataryn{enter}')
+    cy.get('#home').click()
+    cy.url().should('match', urls.datasetSearchPage.base)
+  })
+
   it('API Accessible works', function() {
     cy.route(routes.apiAccessibleFalseDatasets)
     cy.get(apiAccessibleCheckbox).click()
