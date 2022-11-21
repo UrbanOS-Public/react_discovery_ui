3c93805d093f2d2187a88afddbf12aa25d04e1ac
Ian Abbott <ij.abbott2306@gmail.com>
2022-11-21 09:19:22 -0600

    add expect to test

--- src/components/protected-route/protected-route.test.js
+++ src/components/protected-route/protected-route.test.js
@@ -63,6 +63,8 @@ describe('ProtectedRoute component', () => {
       render(<Router><ProtectedRoute exact path='/' component={() => <div>Test Div</div>} /></Router>)
 
       await waitFor(() => screen.getByText('Test Div'))
+
+      expect(loginWithRedirect).not.toHaveBeenCalled()
     })
   })
 })
