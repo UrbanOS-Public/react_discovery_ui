b900549bd55167e271fe240c3c94d497b08cff98
Ian Abbott <ij.abbott2306@gmail.com>
2022-12-02 09:25:49 -0600

    format

--- src/pages/apikey-view/apikey-view.js
+++ src/pages/apikey-view/apikey-view.js
@@ -1,6 +1,6 @@
 import './apikey-view.scss'
 import Modal from 'react-modal'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import ReportProblemOutlinedIcon from '@material-ui/icons/ReportProblemOutlined'
 import LoadingElement from '../../components/generic-elements/loading-element'
 import AlertComponent from '../../components/generic-elements/alert-component'
@@ -8,17 +8,7 @@ import routes from '../../routes'
 import FileCopyIcon from '@material-ui/icons/FileCopy'
 import CloseIcon from '@material-ui/icons/Close'
 
-
-
 const ApiKeyView = ({ apiKey, isLoading, isError, errorMessage, generate, dismissGlobalError }) => {
-  useEffect(() => {
-    // Anything in here is fired on component mount.
-    return () => {
-      // Anything in here is fired on component unmount.
-      // this.props.clearApiKey() # TODO
-    }
-  }, [])
-
   const [modalIsOpen, setIsOpen] = useState(false)
 
   Modal.setAppElement('*')
@@ -42,37 +32,37 @@ const ApiKeyView = ({ apiKey, isLoading, isError, errorMessage, generate, dismis
   const renderModal = () => {
     return (
       <Modal
-        className="apiKey-modal-container"
+        className='apiKey-modal-container'
         isOpen={modalIsOpen}
       >
         <div className='apikey-modal-header-box'>
-          <div className="apiKey-modal-title">
+          <div className='apiKey-modal-title'>
             Generate API Key
           </div>
           <CloseIcon className='apiKey-modal-close-button' onClick={closeModal} />
         </div>
 
-        <hr className="solid"/>
-        <div className="apikey-modal-paragraph">
-          <div className="apikey-modal-warning-icon-container">
-            <ReportProblemOutlinedIcon className="apikey-modal-warning-icon"/>
+        <hr className='solid' />
+        <div className='apikey-modal-paragraph'>
+          <div className='apikey-modal-warning-icon-container'>
+            <ReportProblemOutlinedIcon className='apikey-modal-warning-icon' />
           </div>
-          <div className="apikey-modal-paragraph-text">
+          <div className='apikey-modal-paragraph-text'>
             Please note that generating a new API key will invalidate any existing API configurations. You will need to
             update any system using your API key with the newly generated key.
           </div>
           {
             isLoading && (
-              <div className="loading-spinner-container">
-                <LoadingElement className="loading-spinner"/>
+              <div className='loading-spinner-container'>
+                <LoadingElement className='loading-spinner' />
               </div>
             )
           }
         </div>
-        <hr className="solid"/>
-        <div className="apikey-modal-button-group">
-          <button className="apikey-modal-confirm-button" onClick={generate}>Generate</button>
-          <button className="apikey-modal-cancel-button" onClick={closeModal}>Cancel</button>
+        <hr className='solid' />
+        <div className='apikey-modal-button-group'>
+          <button className='apikey-modal-confirm-button' onClick={generate}>Generate</button>
+          <button className='apikey-modal-cancel-button' onClick={closeModal}>Cancel</button>
         </div>
       </Modal>
     )
@@ -82,17 +72,17 @@ const ApiKeyView = ({ apiKey, isLoading, isError, errorMessage, generate, dismis
     return (
       <div>
         {renderModal()}
-        <div className="apiKey-view-title">
+        <div className='apiKey-view-title'>
           Generate API Key {apiKey}
         </div>
-        <div className="apiKey-view-paragraph">Your API key should be kept secure and never shared. For security
+        <div className='apiKey-view-paragraph'>Your API key should be kept secure and never shared. For security
           reasons,
           we do not display your API key.
         </div>
-        <div className="apiKey-view-paragraph">For first time API Key creation or if you lose your API key, you can
+        <div className='apiKey-view-paragraph'>For first time API Key creation or if you lose your API key, you can
           generate a new key here.
         </div>
-        <button className="apiKey-view-generate-button primary-background-color" onClick={openModal}>Generate</button>
+        <button className='apiKey-view-generate-button primary-background-color' onClick={openModal}>Generate</button>
       </div>
     )
   }
@@ -100,34 +90,34 @@ const ApiKeyView = ({ apiKey, isLoading, isError, errorMessage, generate, dismis
   const renderDisplayPage = () => {
     return (
       <div>
-        <div className="apiKey-view-title">
+        <div className='apiKey-view-title'>
           Your API Key
         </div>
-        <div className="apiKey-view-paragraph">This API key will only be displayed once. Please store it somewhere
+        <div className='apiKey-view-paragraph'>This API key will only be displayed once. Please store it somewhere
           secure. If you lose it, you will need to reset your key to get a new one.
         </div>
-        <div className="apiKey-view-sub-title">
+        <div className='apiKey-view-sub-title'>
           API Key
         </div>
         <div className='apiKey-view-display-page-display-window-container'>
-          <input className="apiKey-view-display-page-display-window" type="text" value={apiKey} readOnly/>
-          <button className="apiKey-view-display-page-copy-button primary-background-color" onClick={copyApiKeyToClipboard}>
+          <input className='apiKey-view-display-page-display-window' type='text' value={apiKey} readOnly />
+          <button className='apiKey-view-display-page-copy-button primary-background-color' onClick={copyApiKeyToClipboard}>
             Copy
             <FileCopyIcon />
           </button>
         </div>
         <div className='apiKey-view-display-page-return-button-container'>
-            <a className='apiKey-view-display-page-return-button-link' href={routes.root}>
-              <button className="apiKey-view-display-page-return-button primary-background-color"> Return Home</button>
-            </a>
+          <a className='apiKey-view-display-page-return-button-link' href={routes.root}>
+            <button className='apiKey-view-display-page-return-button primary-background-color'> Return Home</button>
+          </a>
         </div>
       </div>
     )
   }
 
   return (
-    <div className="apiKey-view-container">
-      <AlertComponent errorMessage={errorMessage} closeFunction={dismissGlobalError} showAlert={isError}/>
+    <div className='apiKey-view-container'>
+      <AlertComponent errorMessage={errorMessage} closeFunction={dismissGlobalError} showAlert={isError} />
       {
         !apiKey && renderGeneratePage()
       }
@@ -136,6 +126,5 @@ const ApiKeyView = ({ apiKey, isLoading, isError, errorMessage, generate, dismis
       }
     </div>
   )
-
 }
 export default ApiKeyView
