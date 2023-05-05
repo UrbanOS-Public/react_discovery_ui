1a715e946f9cc0844eb5d353a8a5fd7b6cea549c
Ian Abbott <ij.abbott2306@gmail.com>
2023-05-05 17:16:34 -0500

    update api_required X to Yes

--- src/components/dataset-api-doc/api-example.js
+++ src/components/dataset-api-doc/api-example.js
@@ -11,6 +11,7 @@ const defaultHeaders = [
 ]
 
 export default ({ title, descriptionHtml, action, url, params, examples, headers = defaultHeaders }) => {
+  console.log(headers)
   return (
     <api-example>
       <div className='example-container'>
@@ -96,7 +97,7 @@ function renderHeader(header) {
       <td>
         <span className='pill'>{header.name}</span>
       </td>
-      <td className="required-marker">{header.required && "X"}</td>
+      <td className="required-marker">{header.required ? "Yes" : "No"}</td>
       <td>{header.default}</td>
       <td>{header.description}</td>
     </tr>
