2b889b0b270fbf5882e96044d97c568a2ede5b31
smithc197 <smithc197@michigan.gov>
2026-07-09 14:05:03 -0400

    Added aria labels to arrow buttons on datasets page. Addresses bug 120584

--- src/components/generic-elements/arrow-left-button/arrow-left-button.js
+++ src/components/generic-elements/arrow-left-button/arrow-left-button.js
@@ -4,7 +4,7 @@ const ENABLED_COLOR = '#000000'
 const DISABLED_COLOR = '#595959'
 
 export default ({ className, disabled = false, onClick = () => { }, innerClass }) => (
-  <button disabled={disabled} className={`${className} ${disabled ? 'disabled' : ''}`} onClick={onClick}>
+  <button disabled={disabled} aria-label="Previous page" className={`${className} ${disabled ? 'disabled' : ''}`} onClick={onClick}>
     <ArrowLeft className={innerClass} height='inherit' fill={disabled ? DISABLED_COLOR : ENABLED_COLOR} accessibilityDesc='Arrow Left' />
   </button>
 )
--- src/components/generic-elements/arrow-right-button/arrow-right-button.js
+++ src/components/generic-elements/arrow-right-button/arrow-right-button.js
@@ -4,7 +4,7 @@ const ENABLED_COLOR = '#000000'
 const DISABLED_COLOR = '#595959'
 
 export default ({ className, disabled = false, onClick = () => { }, innerClass }) => (
-  <button disabled={disabled} className={`${className} ${disabled ? 'disabled' : ''}`} onClick={onClick}>
+  <button disabled={disabled} aria-label="Next page" className={`${className} ${disabled ? 'disabled' : ''}`} onClick={onClick}>
     <ArrowRight className={innerClass} height='inherit' fill={disabled ? DISABLED_COLOR : ENABLED_COLOR} accessibilityDesc='Arrow Right' />
   </button>
 )
